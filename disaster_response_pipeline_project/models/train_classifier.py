c0eb22f09f20acde473f7ccd561afe2dab14d77a
Sergey M <smouzykin@gmail.com>
2019-12-15 20:16:18 -0500

    Add function to save model

--- disaster_response_pipeline_project/models/train_classifier.py
+++ disaster_response_pipeline_project/models/train_classifier.py
@@ -1,16 +1,19 @@
+import nltk
+#nltk.download(['stopwords', 'punkt', 'wordnet', 'averaged_perceptron_tagger',
+#               'maxent_ne_chunker', 'words', 'word2vec_sample'])
+
+from joblib import dump, load
+import pickle
 import sys
-
-
-# import libraries
 import re
 import numpy as np
 import pandas as pd
+import time
 
-import sqlite3
 from sqlalchemy import create_engine
 
 import nltk
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
@@ -18,9 +21,17 @@ from nltk.stem.porter import PorterStemmer
 from nltk import ne_chunk, pos_tag
 
 from sklearn import svm
-from sklearn.linear_model import LogisticRegression, SGDClassifier, RidgeClassifierCV
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, BaggingClassifier
+from sklearn.linear_model import (LogisticRegression,
+                                  RidgeClassifier)
+
+from sklearn.ensemble import (RandomForestClassifier,
+                              BaggingClassifier,
+                              RandomTreesEmbedding
+                              )
+
 from sklearn.multioutput import MultiOutputClassifier
+from sklearn.multiclass import OneVsRestClassifier
+
 
 from sklearn.naive_bayes import GaussianNB, BernoulliNB
 
@@ -28,16 +39,38 @@ from sklearn.model_selection import train_test_split, StratifiedKFold, GridSearc
 from sklearn.preprocessing import PolynomialFeatures
 
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.preprocessing import StandardScaler, RobustScaler, Normalizer, FunctionTransformer
+from sklearn.preprocessing import (StandardScaler, RobustScaler, Normalizer,
+                                   FunctionTransformer, QuantileTransformer,
+                                   PowerTransformer, OneHotEncoder)
+
+from sklearn.compose import ColumnTransformer
 
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, HashingVectorizer, FeatureHasher
-from sklearn.feature_selection import chi2, SelectKBest
-from sklearn.decomposition import PCA, TruncatedSVD
 
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, classification_report, make_scorer
+from sklearn.feature_extraction.text import (CountVectorizer,
+                                             TfidfTransformer,
+                                             HashingVectorizer
+                                             )
+
+from sklearn.feature_selection import chi2, SelectKBest
+from sklearn.decomposition import TruncatedSVD
+
+from sklearn.neighbors import (KNeighborsClassifier,
+                               NeighborhoodComponentsAnalysis)
+
+from sklearn.metrics import (confusion_matrix, f1_score, precision_score,
+                             recall_score, classification_report,
+                             roc_curve, auc, accuracy_score, make_scorer)
 
 from sklearn.utils import resample
+
+from models.custom_tx import (StartingVerbExtractor,
+                              KeywordSearch,
+                              EntityCount,
+                              GetVerbNounCount,
+                              tokenize,
+                              Dense
+                              )
 
 #%%
 
@@ -75,59 +108,6 @@ def load_data(database_filepath, file_type='db'):
     return X, Y, category_names
 
 
-def tokenize(text):
-    """
-
-    """
-
-    # load stopwords
-    stop_words = stopwords.words("english")
-
-    # remove punctuations (retain alphabetical and numeric chars) and convert to all lower case
-    # tokenize resulting text
-    tokens = word_tokenize(re.sub(r"[^a-zA-Z0-9]", ' ', text.lower().strip()))
-
-    # lemmatize and remove stop words
-    lemmatized = [WordNetLemmatizer().lemmatize(word) for word in tokens if word not in stop_words]
-
-    return lemmatized
-
-
-#%%
-
-class SplitNote(BaseEstimator, TransformerMixin):
-    """Split message at the word `Note`, keep message prior to `Note`"""
-    def split_note(self, string):
-        return re.split('Note', string)[0]
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, X):
-        return pd.DataFrame(pd.Series(X).apply(self.split_note))
-
-
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text):
-        try:
-            sentence_list = nltk.sent_tokenize(text)
-            for sentence in sentence_list:
-                pos_tags = nltk.pos_tag(tokenize(sentence))
-                first_word, first_tag = pos_tags[0]
-                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                    return True
-            return False
-        except Exception:
-            return False
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
 #%%
 
 #X, y = load_data('data/disaster_message_cat')
@@ -144,17 +124,18 @@ def grid_search(model, params, X_train=X_train, y_train=y_train):
         model,
         params,
         cv=3,
+        scoring='f1_weighted',
         n_jobs=-1,
     )
     grid_cv.fit(X_train.ravel(), y_train)
     return grid_cv
 
-def build_model(clf, params):
+def build_model():
 
     svc_params = dict(
-        C = 1,
-        kernel = 'sigmoid',
-        cache_size = 2000,
+        C = 2,
+        kernel = 'linear',
+        cache_size = 1000,
         class_weight = 'balanced',
         random_state = 11
 
@@ -164,53 +145,74 @@ def build_model(clf, params):
     clf = svm.SVC(**svc_params)
 
     pipeline = Pipeline([
-        ('preprocess', SplitNote()),
-
-        ('features', FeatureUnion([
-
-            ('text_pipeline', Pipeline([
-
-                ('count_vect', CountVectorizer(tokenizer=tokenize,
-                                               ngram_range=(1,2),
-                                               max_df=0.9,
-                                               min_df=0.02,
-                                               max_features=500
-                                              )
-                ),
-                ('tfidf_tx', TfidfTransformer()),
-                ('decomp', TruncatedSVD(n_components=10, random_state=11))
-            ])),
-            ('extract_verb', StartingVerbExtractor()),
-
-        ], n_jobs=-1)),
-
+        ('count_vect', CountVectorizer(
+                tokenizer=tokenize,
+                ngram_range=(1, 2),
+                max_features=300
+                )),
+        ('tfidf_tx', TfidfTransformer()),
         ('clf', MultiOutputClassifier(clf, n_jobs=-1))
     ])
+
     return pipeline
 
 
 
 
 def evaluate_model(model, x_test, y_test, y_pred, category_names):
-
+    y_pred = model.predict(x_test)
     # print label and f1-score for each
-    scores = []
+    avg = 'weighted'
+    f1 = []
+    prec = []
+    rec = []
+    acc = []
+    #train_scores = []
     for i in range(y_test[:, :].shape[1]):
-        scores.append(f1_score(y_test[:, i], y_pred[:, i]))
-    for lbl, scr in zip(category_names, scores):
-        print(lbl, ':', np.round(scr, 4))
+        f1.append(f1_score(y_test[:, i], y_pred[:, i], average=avg))
+        acc.append(accuracy_score(y_test[:, i], y_pred[:, i]))
+        rec.append(recall_score(y_test[:, i], y_pred[:, i], average=avg))
+        prec.append(precision_score(y_test[:, i], y_pred[:, i], average=avg))
 
+    # summarize f1-scores and compare to the rate of positive class occurance
+    f1_df = pd.DataFrame({'f1-score': np.round(f1, 4),
+                          'precision': np.round(prec, 4),
+                          'recall': np.round(rec, 4),
+                          'accuracy': np.round(acc, 4)}, index=category_names)
+
+    # print results
     print('\n')
-    print('Average across all labels:', sum(scores) / len(scores))
+    print('='*75)
+    print(f1_df)
+    print('\n')
+    print(f1_df.agg(['mean', 'median', 'std']))
+    print('='*75)
     print('\n')
 
-    # summarize f1-scores in DF
-    f1_df = pd.DataFrame({'f1-score': scores}, index=category_names)
-    return f1_df
 
 
-def save_model(model, model_filepath):
-    pass
+
+def save_model(model, filepath):
+    """
+
+
+    Parameters
+    ----------
+        model : estimator
+
+        filepath : save model to this directory
+
+    Returns
+    -------
+        None.
+
+    """
+    try:
+        dump(model, filepath)
+    except Exception as e:
+        print(e)
+        print('Failed to pickle model')
+
 
 
 def main():
